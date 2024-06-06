08a56a3d0e426bf89ea73d4a8908a3f281d1223f
Yuutaleo <leo.yuta2004@gmail.com>
2024-06-06 01:26:31 -0300

    header responsivo

--- my-app/src/app/components/Header.js
+++ my-app/src/app/components/Header.js
@@ -1,3 +1,56 @@
+// import React, { useState } from 'react';
+// import styles from '../styles/Header.module.css';
+// import { FaBars, FaTimes, FaHome, FaHistory } from 'react-icons/fa';
+// import { IoPersonCircleOutline } from 'react-icons/io5';
+// import { BiSolidVideos } from 'react-icons/bi';
+
+// export default function Header() {
+//   const [menuOpen, setMenuOpen] = useState(false);
+
+//   const toggleMenu = () => {
+//     setMenuOpen(!menuOpen);
+//   };
+
+//   return (
+//     <header className={styles.header}>
+//       <div className={styles.leftSection}>
+//         <button className={styles.menuButton} onClick={toggleMenu}>
+//           <FaBars />
+//         </button>
+//         <a href="#home" className={styles.removersub}>
+//           <img className={styles.logo} src="/images/logo2.svg" alt="Logo Itube" />
+//         </a>
+//       </div>
+//       <input type="text" placeholder="Pesquisar" className={styles.searchBar} />
+//       <div className={styles.rightSection}>
+//         <a href="#upload" className={styles.removersub}>
+//           <div className={styles.uploadButton}>Upload</div>
+//         </a>
+//         <a href="#perfil" className={styles.removersub}>
+//           <div className={styles.profileButton}>Perfil</div>
+//         </a>
+//       </div>
+//       <nav className={`${styles.menu} ${menuOpen ? styles.open : ''}`}>
+//         <button className={styles.closeButton} onClick={toggleMenu}>
+//           <FaTimes />
+//         </button>
+//         <a href="#Início" className={styles.removersub}>
+//           <FaHome className={styles.menuIcon} /> Início
+//         </a>
+//         <a href="#Canal" className={styles.removersub}>
+//           <IoPersonCircleOutline className={styles.menuIcon} /> Canal
+//         </a>
+//         <a href="#Historico" className={styles.removersub}>
+//           <FaHistory className={styles.menuIcon} /> Histórico
+//         </a>
+//         <a href="#Meus-Vídeos" className={styles.removersub}>
+//           <BiSolidVideos className={styles.menuIcon} /> Meus Vídeos
+//         </a>
+//       </nav>
+//     </header>
+//   );
+// }
+
 import React, { useState } from 'react';
 import styles from '../styles/Header.module.css';
 import { FaBars, FaTimes, FaHome, FaHistory } from 'react-icons/fa';
